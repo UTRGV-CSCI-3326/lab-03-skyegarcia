78ded78cad6ef11f18061a5bc4f84a5f538ed4f9
skyegarcia <161562153+skyegarcia@users.noreply.github.com>
2025-01-31 02:25:29 +0000

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,15 @@
+public class BankAccount{
+    public static void main(String[] args){
+    float total = 2175.37f;
+    total -= 302.50;
+    total += 50.03;
+    total /= 2;
+    total += 20;
+    total -= 1;
+    total *= 2;
+    total += 1;
+
+    System.out.printf("%.2f%n", total);
+    }
+}
 
